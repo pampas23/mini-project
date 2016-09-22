11fb25a58896435adeba3631b6527d9a9d31cda2
Pampas Chen <pampas23@gmail.com>
2016-09-22 22:34:03 +0800

    New & Show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,28 @@
 class PlayersController < ApplicationController
 
-def index
-	@players=Player.all
-end
+	def index
+		@players=Player.all
+	end
 
+	def new
+		@player = Player.new
+	end
+
+	def create
+		@player = Player.new(player_params)
+  		@player.save
+
+  		redirect_to :action => :index
+	end
+
+	def show
+		@player = Player.find(params[:id])
+	end
+
+
+	private
+
+	def player_params
+  		params.require(:player).permit(:name, :rank)
+	end
 end
